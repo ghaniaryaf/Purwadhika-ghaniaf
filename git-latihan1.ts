ab3f2200f9da8d30eb6ffb856d710f5b7af016b6
ghaniaryaf <tyuiop.2334@gmail.com>
2025-07-28 15:11:40 +0700

    update another line

--- git-latihan1.ts
+++ git-latihan1.ts
@@ -1,3 +1,4 @@
 console.log("Hello, Git!");
 console.log("This is a TypeScript file for Git latihan.");
 console.log("Let's learn version control with Git.");
+console.log("This is an updated file for Git latihan.");
--- git-latihan2.ts
+++ git-latihan2.ts
@@ -2,3 +2,5 @@ console.log("Hello, Git!");
 console.log("This is a TypeScript file for Git latihan.");
 console.log("Let's learn version control with Git.");
 console.log("This is an updated file for Git latihan.");
+console.log("This is an additional line for further updates.");
+console.log("This is another update to the file for Git latihan.");
